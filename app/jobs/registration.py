267b1e2a158fd59b8671efd6196d496b28c1459c
ShanCX7 <emwaka14@gmail.com>
2024-11-18 15:39:43 +0300

    Added training session and attendance plus other functionalities in registration

--- app/jobs/registration.py
+++ app/jobs/registration.py
@@ -1,6 +1,7 @@
 # from app.utils.salesforce_client import SalesforceClient
 import asyncio
 from utils.salesforce_client import upsert_to_salesforce
+from training_session_and_attendance_update import process_attendance, process_training_session
   
 # Process functions for each Salesforce object
 def process_training_group(data, sf_connection):
@@ -21,6 +22,7 @@ def process_household(data, sf_connection):
     survey_detail = data.get('form', {}).get('survey_detail')
     primary_member = data.get("form", {}).get("Primary_Household_Member") == "Yes"
 
+    # 1. Farmer Registration Form
     if survey_detail in ["New Farmer New Household", "New Farmer Existing Household", "Existing Farmer Change in FFG"]:
  
         household_fields = {
@@ -38,11 +40,28 @@ def process_household(data, sf_connection):
                 household_fields,
                 sf_connection
             )
+            
+    # 2. Farmer Update Form       
+    elif survey_detail == 'Participant Update':
+        household_fields = {
+            "Farm_Size__c": get_farm_size(data) or None,
+            "Number_of_Coffee_Plots__c": get_number_of_plots(data) or None
+        }
+        
+        if primary_member:
+            upsert_to_salesforce(
+                "Household__c",
+                "Household_ID__c",
+                data.get("form").get("Household_Id"),
+                household_fields,
+                sf_connection
+            )
 
 # Registration Fields for New Farmers
 def process_participant(data, sf_connection):
     survey_detail = data.get('form', {}).get('survey_detail')
     
+    # 1. New Farmer Registration
     if survey_detail in ["New Farmer New Household", "New Farmer Existing Household"]:
         participant_fields = {
             # Relationships and Main IDs
@@ -74,6 +93,45 @@ def process_participant(data, sf_connection):
             "Participant__c",
             "CommCare_Case_Id__c",
             data.get("form", {}).get("subcase_0", {}).get("case", {}).get("@case_id"),
+            participant_fields,
+            sf_connection
+        )
+    
+    # 2. Existing Farmer Change in FFG
+    elif survey_detail == "Existing Farmer Change in FFG":
+        participant_fields = {
+            # Relationships and Main IDs
+            "TNS_Id__c": data.get("form", {}).get("Farmer_Id"),
+            "Household__r":{
+                "Household_ID__c": data.get("form").get("Household_Id")
+            },
+            "Training_Group__c": data.get("form", {}).get("Training_Group_Id"),
+            "Former_Training_Group__c": data.get("form", {}).get("existing_farmer_change_in_ffg", {}).get("old_farmer_ffg"),
+            "Updated_Date__c": data.get("form", {}).get("registration_date") or None,
+            "Create_In_CommCare__c": True
+        }
+        upsert_to_salesforce(
+            "Participant__c",
+            "CommCare_Case_Id__c",
+            data.get("form", {}).get("existing_farmer_change_in_ffg", {}).get("old_farmer_id"),
+            participant_fields,
+            sf_connection
+        )
+    
+    # 3. Existing Farmer Updating Details
+    elif survey_detail == "Participant Update":
+        participant_fields = {
+            "Farm_Size__c": get_farm_size(data),
+            "Phone_Number__c": data.get("form", {}).get("Phone_Number"),
+            "Number_of_Coffee_Plots__c": get_number_of_plots(data),
+            "Other_Id_Number__c": get_other_id_number(data),
+            "Updated_Date__c": data.get("form", {}).get("registration_date") or None,
+            "Create_In_CommCare__c": False
+        }
+        upsert_to_salesforce(
+            "Participant__c",
+            "CommCare_Case_Id__c",
+            data.get("form", {}).get("case", {}).get("@caseid"),
             participant_fields,
             sf_connection
         )
@@ -116,6 +174,8 @@ async def send_to_salesforce(data, sf_connection):
     process_household(data, sf_connection)
     process_participant(data, sf_connection)
     process_participant_deactivation(data, sf_connection)
+    process_training_session(data, sf_connection)
+    process_attendance(data, sf_connection)
 
     return True
 
@@ -129,13 +189,14 @@ def get_farm_size(data):
 
 def get_number_of_plots(data):
     survey_detail = data.get('form', {}).get('survey_detail')
-    if survey_detail != "Existing Farmer Change in FFG":
+    country = data.get("form", {}).get("coffee_project_country") == 'Burundi'
+    if country and survey_detail != "Existing Farmer Change in FFG":
         return data.get("form", {}).get("Number_of_Plots") or None
-    elif data.get("form", {}).get("coffee_project_country") == 'Burundi' and \
-         data.get("form", {}).get("existing_farmer_change_in_ffg", {}).get("former_farmer_primary_secondary_yn") == "Yes":
+    elif country and data.get("form", {}).get("existing_farmer_change_in_ffg", {}).get("former_farmer_primary_secondary_yn") == "Yes":
         return data.get("form", {}).get("existing_farmer_change_in_ffg", {}).get("former_farmer_coffeeplots") or None
     return None
 
+# Processing other ID number
 def get_other_id_number(data):
     survey_detail = data.get('form', {}).get('survey_detail')
     country = data.get("form", {}).get("coffee_project_country")
---
+++ app/jobs/training_session_and_attendance_update.py
@@ -0,0 +1,69 @@
+from utils.salesforce_client import upsert_to_salesforce
+
+# Update the Training Session Object
+def process_training_session(data, sf_connection):
+    form_name = data.get("form",{}).get("@name")
+    
+    # 1. Process for Participant Registration
+    survey_detail = data.get("form",{}).get("survey_detail")
+    if survey_detail in ["New Farmer New Household", "New Farmer Existing Household", "Existing Farmer Change in FFG"]:
+        training_session_fields = {
+            "Trainer__c" : data.get("form", {}).get("trainer"),
+            "Updated_from_CommCare__c": True,
+            "Date__c": data.get("form", {}).get("registration_date")
+        }
+        upsert_to_salesforce(
+            "Training_Session__c",
+            "CommCare_Case_Id__c",
+            data.get("form", {}).get("training_session"),
+            training_session_fields,
+            sf_connection
+        )
+    # 2. Process for Attendance Light
+    else:
+        None
+    
+def process_attendance(data, sf_connection):
+    survey_detail = data.get("form",{}).get("survey_detail")
+    
+    # 1. Process for Farmer Registration - New Farmer
+    if survey_detail in ["New Farmer New Household", "New Farmer Existing Household"]:
+        attendance_fields = {
+            "Status__c": "Present",
+            "Training_Session__r": {
+                "CommCare_Case_Id__c": data.get("form", {}).get("training_session")
+            },
+            "Participant__r": {
+                "CommCare_Case_Id__c": data.get("form", {}).get("subcase_0", {}).get("case", {}).get("@caseid")
+            }
+        }
+        upsert_to_salesforce (
+            "Attendance__c",
+            "Submission_ID__c",
+            data.get("form", {}).get("training_session") + data.get("form", {}).get("subcase_0", {}).get("case", {}).get("@caseid"),
+            attendance_fields,
+            sf_connection
+        )
+        
+    # 2. Process for Farmer Registraion - Existing Farmer
+    elif survey_detail == "Existing Farmer Change in FFG":
+        attendance_fields = {
+            "Status__c": "Present",
+            "Training_Session__r": {
+                "CommCare_Case_Id__c": data.get("form", {}).get("training_session")
+            },
+            "Participant__r": {
+                "CommCare_Case_Id__c": data.get("form", {}).get("existing_farmer_change_in_ffg", {}).get("old_farmer_id")
+            }
+        }
+        upsert_to_salesforce (
+            "Attendance__c",
+            "Submission_ID__c",
+            data.get("form", {}).get("training_session") + data.get("form", {}).get("existing_farmer_change_in_ffg", {}).get("old_farmer_id"),
+            attendance_fields,
+            sf_connection
+        )
+    
+    # 3. Process for Attendance Full - Current Module
+    else:
+        None
