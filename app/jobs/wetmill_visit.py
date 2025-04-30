c60f24120664ad4c5a0ffa0cb270c0a8ff3c4124
Mugenga <rockyyves@gmail.com>
2025-04-30 10:15:12 +0200

    Push

--- app/jobs/wetmill_visit.py
+++ app/jobs/wetmill_visit.py
@@ -25,106 +25,106 @@ ALLOWED_SURVEYS = [
 
 def save_form_visit(data, wetmill_id=None, user_id=None):
     session = SessionLocal()
-#try:
-    form = data.get("form", {})
-    case = form.get("case", {})
-    case_id = case.get("@case_id")
+    try:
+        form = data.get("form", {})
+        case = form.get("case", {})
+        case_id = case.get("@case_id")
 
-    # Lookup/create Wetmill
-    if case_id:
-        wetmill = session.query(Wetmill).filter_by(commcare_case_id=case_id).first()
-        if not wetmill:
-            wetmill = Wetmill(commcare_case_id=case_id, wet_mill_unique_id="", name="Placeholder")
-            session.add(wetmill)
-            session.flush()
-        form_wetmill_id = wetmill.id
-    else:
-        form_wetmill_id = wetmill_id
+        # Lookup/create Wetmill
+        if case_id:
+            wetmill = session.query(Wetmill).filter_by(commcare_case_id=case_id).first()
+            if not wetmill:
+                wetmill = Wetmill(commcare_case_id=case_id, wet_mill_unique_id="", name="Placeholder")
+                session.add(wetmill)
+                session.flush()
+            form_wetmill_id = wetmill.id
+        else:
+            form_wetmill_id = wetmill_id
 
-    visit_date_str = form.get("date")
-    visit_date = datetime.datetime.strptime(visit_date_str, "%Y-%m-%d") if visit_date_str else None
-    form_name = form.get("survey_type")
-    loc_str = form.get("introduction", {}).get("gps", "")
-    point = extract_location_string(loc_str)
+        visit_date_str = form.get("date")
+        visit_date = datetime.datetime.strptime(visit_date_str, "%Y-%m-%d") if visit_date_str else None
+        form_name = form.get("survey_type")
+        loc_str = form.get("introduction", {}).get("gps", "")
+        point = extract_location_string(loc_str)
 
-    entrance_photograph = f'https://www.commcarehq.org/a/{data.get("domain")}/api/form/attachment/{data.get("form", {}).get("meta", {}).get("instanceID")}/{data.get("form", {}).get("introduction", {}).get("wetmill_entrance_photograph")}' if data.get("form", {}).get("introduction", {}).get("wetmill_entrance_photograph") else None
-    
-    form_visit = FormVisit(
-        wetmill_id=form_wetmill_id,
-        user_id=wetmill.user_id,
-        form_name=form_name,
-        visit_date=visit_date,
-        entrance_photograph=entrance_photograph,
-        geo_location=from_shape(point, srid=4326) if point else None,
-    )
-    session.add(form_visit)
-
-    surveys = form.get("surveys", {})
-    completed = form.get("completed_surveys", {})
-    for survey_name, content in surveys.items():
-        if survey_name not in ALLOWED_SURVEYS:
-            print('skipping survey: ' + survey_name)
-            continue
-        if not isinstance(content, dict): # Skip non survey 
-            continue
-
-        # Apply transformation functions (ETL) for each survey
-        transform_func = SURVEY_TRANSFORMATIONS.get(survey_name)
-
-        if transform_func:
-            content = transform_func(content)
-
-        # Determine completed date
-        date_key = f"{survey_name}_date"
-        completed_date = None
-        date_str = completed.get(date_key)
-        if date_str:
-            completed_date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
-
-        general_feedback = content.get("general_feedback")
-
-        survey_response = SurveyResponse(
-            form_visit=form_visit,
-            survey_type=survey_name,
-            completed_date=completed_date,
-            general_feedback=general_feedback
+        entrance_photograph = f'https://www.commcarehq.org/a/{data.get("domain")}/api/form/attachment/{data.get("form", {}).get("meta", {}).get("instanceID")}/{data.get("form", {}).get("introduction", {}).get("wetmill_entrance_photograph")}' if data.get("form", {}).get("introduction", {}).get("wetmill_entrance_photograph") else None
+        
+        form_visit = FormVisit(
+            wetmill_id=form_wetmill_id,
+            user_id=wetmill.user_id,
+            form_name=form_name,
+            visit_date=visit_date,
+            entrance_photograph=entrance_photograph,
+            geo_location=from_shape(point, srid=4326) if point else None,
         )
-        session.add(survey_response)
+        session.add(form_visit)
 
-        # Insert question responses
-        for section, sec_content in content.items():
-            # handle multiple answers questions on top level
-            if isinstance(sec_content, list):
-                for item in sec_content:
-                    add_question_response(session, survey_response, section, section, item)
-            # Nested questions
-            elif isinstance(sec_content, dict):
-                for q_name, ans in sec_content.items():
-                    # handle multiple answers questions in nested
-                    if isinstance(ans, list):
-                        for item in ans:
-                            add_question_response(session, survey_response, section, q_name, item)
-                    else:
-                        # Skip label fields
-                        if q_name.endswith('_label'):
-                            continue
-                        add_question_response(session, survey_response, section, q_name, ans)
-            # Single value questions or flags
-            else:
-                # Skip top-level label or survey keys
-                if section.endswith('_label') or section.startswith('survey_'):
-                    continue
-                add_question_response(session, survey_response, None, section, sec_content)
-    session.commit()
-    return True, None
-#except SQLAlchemyError as e:
-#    session.rollback()
-#    return False, str(e)
-# except Exception as e:
-#   session.rollback()
-#    return False, str(e)
-#finally:
-#    session.close()
+        surveys = form.get("surveys", {})
+        completed = form.get("completed_surveys", {})
+        for survey_name, content in surveys.items():
+            if survey_name not in ALLOWED_SURVEYS:
+                print('skipping survey: ' + survey_name)
+                continue
+            if not isinstance(content, dict): # Skip non survey 
+                continue
+
+            # Apply transformation functions (ETL) for each survey
+            transform_func = SURVEY_TRANSFORMATIONS.get(survey_name)
+
+            if transform_func:
+                content = transform_func(content)
+
+            # Determine completed date
+            date_key = f"{survey_name}_date"
+            completed_date = None
+            date_str = completed.get(date_key)
+            if date_str:
+                completed_date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+
+            general_feedback = content.get("general_feedback")
+
+            survey_response = SurveyResponse(
+                form_visit=form_visit,
+                survey_type=survey_name,
+                completed_date=completed_date,
+                general_feedback=general_feedback
+            )
+            session.add(survey_response)
+
+            # Insert question responses
+            for section, sec_content in content.items():
+                # handle multiple answers questions on top level
+                if isinstance(sec_content, list):
+                    for item in sec_content:
+                        add_question_response(session, survey_response, section, section, item)
+                # Nested questions
+                elif isinstance(sec_content, dict):
+                    for q_name, ans in sec_content.items():
+                        # handle multiple answers questions in nested
+                        if isinstance(ans, list):
+                            for item in ans:
+                                add_question_response(session, survey_response, section, q_name, item)
+                        else:
+                            # Skip label fields
+                            if q_name.endswith('_label'):
+                                continue
+                            add_question_response(session, survey_response, section, q_name, ans)
+                # Single value questions or flags
+                else:
+                    # Skip top-level label or survey keys
+                    if section.endswith('_label') or section.startswith('survey_'):
+                        continue
+                    add_question_response(session, survey_response, None, section, sec_content)
+        session.commit()
+        return True, None
+    except SQLAlchemyError as e:
+        session.rollback()
+        return False, str(e)
+    except Exception as e:
+        session.rollback()
+        return False, str(e)
+    finally:
+        session.close()
 
 def extract_location_string(location_string):
     """
